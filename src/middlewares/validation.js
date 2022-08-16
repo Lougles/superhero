f247ab52b12c99fee19ad41cc66dc135d466e7d2
vovacelidze <louglescom@gmail.com>
2022-08-16 19:43:53 +0300

    added validation for update method

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -2,16 +2,32 @@ const {validationError} = require('../helpers/errors');
 const Joi = require('joi');
 
 
-const schema = Joi.object({
+const addHeroSchema = Joi.object({
   nickname: Joi.string().alphanum().min(3).max(15).required(),
   real_name: Joi.string().alphanum().min(3).max(15).required(),
-  origin_description: Joi.string(),
-  superpowers: Joi.string(),
-  catch_phrase: Joi.string()
-})
+  origin_description: Joi.string().min(10).max(300),
+  superpowers: Joi.string().min(5).max(100),
+  catch_phrase: Joi.string().min(5).max(100),
+});
+
+const updateHeroSchema = Joi.object({
+  nickname: Joi.string().alphanum().min(3).max(15),
+  real_name: Joi.string().alphanum().min(3).max(15),
+  origin_description: Joi.string().min(10).max(300),
+  superpowers: Joi.string().min(5).max(100),
+  catch_phrase: Joi.string().min(5).max(100),
+});
 
 const addHeroValidation = (req, res, next) => {
-  const result = schema.validate(req.body);
+  const result = addHeroSchema.validate(req.body);
+  if(result.error){
+    next (new validationError(result.error));
+  }
+  next();
+}
+
+const updateHeroValidation = (req, res, next) => {
+  const result = updateHeroSchema.validate(req.body);
   if(result.error){
     next (new validationError(result.error));
   }
@@ -19,5 +35,6 @@ const addHeroValidation = (req, res, next) => {
 }
 
 module.exports = {
-  addHeroValidation
+  addHeroValidation,
+  updateHeroValidation
 }
