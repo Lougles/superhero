1dcba17cb157854b482b7a3b262a58e178c6a9d7
vovacelidze <louglescom@gmail.com>
2022-08-17 21:54:32 +0300

    test for service getherobyid wrong id is done

--- __tests__/HeroService.test.js
+++ __tests__/HeroService.test.js
@@ -1,23 +1,24 @@
+const { ObjectId } = require('mongodb');
 const {Hero} = require('../src/db/superheroModel');
+const { WrongIdError } = require('../src/helpers/errors');
 const {
   getHeroByIdService,
+  getHeroService
 } = require('../src/services/heroService');
 
 
 describe('Hero Service test', () => {
+  const mHeroId = '1';
+  const mockHero = {
+    _id: mHeroId,
+    nickname: "Superhero",
+    real_name: "Clark Kent",
+    origin_description: "this is a cool hero",
+    superpowers: 'solar flare',
+    catch_phrase: 'it is a good day for good things',
+    image: '//www.avatar.com/avatar/d415f0e4t6471dfdd9bc4f827329ef48',
+  };
   test('should return hero by id', async() => {
-
-    const mHeroId = '1';
-    const mockHero = {
-      _id: mHeroId,
-      nickname: "Superhero",
-      real_name: "Clark Kent",
-      origin_description: "this is a cool hero",
-      superpowers: 'solar flare',
-      catch_phrase: 'it is a good day for good things',
-      image: '//www.avatar.com/avatar/d415f0e4t6471dfdd9bc4f827329ef48',
-    };
-
     jest.spyOn(Hero, 'findOne').mockImplementationOnce(() => mockHero)
     const result = await getHeroByIdService(mHeroId)
     expect(result._id).toEqual(mHeroId);
@@ -28,5 +29,11 @@ describe('Hero Service test', () => {
     expect(result.catch_phrase).toBeDefined();
     expect(result.image).toBeDefined();
   })
-  test
+  test('should error', async() => {
+    const id = '2';
+    const error = `Fail, id ${id} is not found`;
+    jest.spyOn(Hero, 'findOne').mockImplementationOnce(() => error)
+    const result = await getHeroByIdService(id);
+    expect(result).toEqual(`Fail, id ${id} is not found`)
+  })
 })
